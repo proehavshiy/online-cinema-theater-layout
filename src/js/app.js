a4bb972ab9dee3bc19a051e09b23d5196e57a26a
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-04-12 15:53:12 +0300

    feat: hide aside block

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,12 @@ window.addEventListener('scroll', navbar.fixedNav)
 
 // webp
 browserWebp.isWebp()
+
+
+// сайд-блок
+const aside = document.querySelector('.control-bar')
+const asideButton = aside.querySelector('.control-bar__button')
+
+asideButton.addEventListener('click', () => {
+  aside.classList.toggle('control-bar_open')
+})
