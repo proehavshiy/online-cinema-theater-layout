2cf62fbf736024fbe4f02d5b0f620243a85336e6
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-04-13 19:11:27 +0300

    feat: aside button rotation

--- src/js/app.js
+++ src/js/app.js
@@ -14,16 +14,13 @@ browserWebp.isWebp()
 // сайд-блок
 const aside = document.querySelector('.control-bar')
 const asideButton = aside.querySelector('.control-bar__button')
-
-
-
-// смена ширины карточек в зависимости от ширины блока main 
 const main = document.querySelector('.main')
-
-
 
 asideButton.addEventListener('click', () => {
   aside.classList.toggle('control-bar_open')
+
+  // разворот кнопки
+  asideButton.classList.toggle('control-bar__button_open')
 
   // уменьшение/увеличение базового шрифта при открытии/закрытии aside
   main.classList.toggle('main_collapsed')
